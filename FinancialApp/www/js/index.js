24a5ce9e87704fce748f136747f457a9e55c2afc
Chukwuma Akunyili <chukwuma.akunyili.233279@unn.edu.ng>
2024-03-26 21:11:57 +0100

    Update FinancialApp with interactive features and OpenBankProject API integration
    Refined the existing FinancialApp project by extending features in index.html and index.js, changing its purpose from a hello world app to an interactive financial application. The index.js now contains AJAX calls to the OpenBankProject API, providing functionalities like user sign-in, retrieving bank info, fetching user accounts and transactions. Furthermore, added jQuery and dotenv dependencies to enrich application functionality and enhance environment variable management.

--- FinancialApp/www/js/index.js
+++ FinancialApp/www/js/index.js
@@ -19,11 +19,132 @@
 
 // Wait for the deviceready event before using any of Cordova's device APIs.
 // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-document.addEventListener('deviceready', onDeviceReady, false);
+let token;
+// Check if jQuery is loaded
+if (typeof jQuery == 'undefined') {
+    console.log("jQuery is not loaded. Please make sure to load jQuery before this script.");
+} else {
+    $(document).ready(function () {
+        document.addEventListener('deviceready', onDeviceReady, false);
 
-function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
+        function onDeviceReady() {
+            document.getElementById("button1").addEventListener("click", signIn);
+            console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+            document.getElementById("button2").addEventListener("click", retrieveBankInfo);
+            document.getElementById("button3").addEventListener("click", fetchAccountsAtABanks);
+            document.getElementById('button4').addEventListener("click", fetchTransactions(1995));
+            document.getElementById('deviceready').classList.add('ready');
 
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
+        }
+
+        function signIn() {
+            console.log("in function signIn");
+            document.getElementById("text1").innerHTML = "Signing in";
+
+            $.ajax({
+                url: "https://apisandbox.openbankproject.com/my/logins/direct",
+                type: "POST",
+                dataType: "json",
+                crossDomain: true,
+                cache: false,
+                contentType: "application/json; charset=utf-8",
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader("Authorization", 'DirectLogin username="susan.uk.29@example.com", password="2b78e8", consumer_key="oa5ppybnxquwbdhccdlxcikasrhteyyg2rcdx2nw"');
+                },
+                success: function (data, textStatus, jQxhr) {
+                    console.log("in sign-in success");
+                    document.getElementById("text1").innerHTML = "Successful login. Token: " + data.token;
+                    token = data.token;
+                    retrieveBankInfo();
+                },
+                error: function (jqXhr, textStatus, errorThrown) {
+                    console.log("in sign-in error");
+                    console.log("HTTP status code: " + jqXhr.status);
+                    console.log("Error thrown: " + errorThrown);
+                    console.log("Server response: " + jqXhr.responseText);
+                    document.getElementById("text1").innerHTML = "error";
+                }
+            });
+        }
+    });
+}
+
+function retrieveBankInfo() {
+    $.ajax({
+        url: "https://apisandbox.openbankproject.com/obp/v4.0.0/banks/gh.29.uk",
+        type: "GET",
+        dataType: "json",
+        crossDomain: true,
+        cache: false,
+        contentType: "application/json; charset=utf-8",
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader("Authorization", 'DirectLogin token=' + token);
+        },
+        success: function (data) {
+            console.log("in retrieve bank info success");
+            console.log(data);
+            document.getElementById("text2").innerHTML = "Retrieve Bank Information  successful";
+        },
+        error: function (jqXhr, textStatus, errorThrown) {
+            console.log("in retrieve bank info error");
+            document.getElementById("text2").innerHTML = "Retrieve Bank Information failed";
+            console.log("HTTP status code: " + jqXhr.status);
+            console.log("Error thrown: " + errorThrown);
+            console.log("Server response: " + jqXhr.responseText);
+        }
+    });
+}
+
+function fetchAccountsAtABanks() {
+    $.ajax({
+        url: "https://apisandbox.openbankproject.com/obp/v4.0.0/banks/gh.29.uk/accounts/private",
+        type: "GET",
+        dataType: "json",
+        crossDomain: true,
+        cache: false,
+        contentType: "application/json; charset=utf-8",
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader("Authorization", 'DirectLogin token=' + token);
+        },
+        success: function (data) {
+            console.log("in fetch accounts at a banks success");
+            console.log(data);
+            document.getElementById("text3").innerHTML = "Fetch Accounts Held By A User At A Banks successful";
+        },
+        error: function (jqXhr, textStatus, errorThrown) {
+            console.log("in fetch accounts at a banks error");
+            document.getElementById("text3").innerHTML = "Fetch Accounts Held By A User At A Banks successful failed";
+            console.log("HTTP status code: " + jqXhr.status);
+            console.log("Error thrown: " + errorThrown);
+            console.log("Server response: " + jqXhr.responseText);
+        }
+    });
+}
+
+function fetchTransactions(accountId) {
+    setTimeout(() => {
+        $.ajax({
+            url: `https://apisandbox.openbankproject.com/obp/v4.0.0/my/banks/gh.29.uk/accounts/${accountId}/transactions`,
+            type: "GET",
+            dataType: "json",
+            crossDomain: true,
+            cache: false,
+            contentType: "application/json; charset=utf-8",
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader("Authorization", 'DirectLogin token=' + token);
+            },
+            success: function (data) {
+                console.log("in get user bank transactions success");
+                console.log(data);
+                document.getElementById("text4").innerHTML = "Fetch User Bank Transactions successful";
+            },
+            error: function (jqXhr, textStatus, errorThrown) {
+                console.log("in get user bank transaction error");
+                document.getElementById("text4").innerHTML = "Fetch User Bank Transactions failed";
+                console.log("HTTP status code: " + jqXhr.status);
+                console.log("Error thrown: " + errorThrown);
+                console.log("Server response: " + jqXhr.responseText);
+            }
+        });
+    }, 2000);
 }
